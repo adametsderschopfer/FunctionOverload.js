a3686209327a0c9761a82395ea16816c4e41af98
Adamets Vladislav <v.adamets@consult-info.ru>
2021-03-01 23:20:02 +0200

    Version overload with type checking

--- src/functionOverload.ts
+++ src/functionOverload.ts
@@ -1,4 +1,76 @@
-type TOverloadArguments = Function
+import instantiate = WebAssembly.instantiate;
+
+type TOverloadArguments = Function | [object, Function];
+
+function overload() {
+	const args: Array<TOverloadArguments> = Array.from<TOverloadArguments>(arguments);
+	let functions: Array<TOverloadArguments> = [];
+
+	for (let i = 0; i !== args.length; i++) {
+		if (typeof (args[i]) === 'function') {
+			functions[args[i].length] = args[i];
+		}
+
+		if (args[i] instanceof Array) {
+			const argArr = args[i];
+
+			if (argArr[0] instanceof Object && argArr[1] instanceof Function) {
+				functions[args[i][1].length] = args[i];
+			}
+		}
+
+		/*		if (typeof (args[i]) === 'function') {
+			functions[args[i].length] = args[i];
+		}*/
+	}
+
+	return function () {
+		const callElement = functions[arguments.length];
+
+		if (callElement instanceof Function) {
+			functions[arguments.length]?.apply(this, arguments)
+		} else if (callElement instanceof Array) {
+			const typesOfCallElement = Object.values(callElement[0]);
+			let call = false;
+
+			const matchingTypes = Array.from(arguments).map((arg, idx) => {
+				if (typeof (arg) === typesOfCallElement[idx].toLowerCase()) {
+					return '1';
+				} else {
+					return '0'
+				}
+			})
+				.find((i: string) => i === "0");
+
+			if (matchingTypes == undefined) {
+				functions[arguments.length][1].apply(this, arguments)
+			} else {
+				console.error(SyntaxError(`You have a mistake in your arguments type!`));
+			}
+		}
+	}
+}
+
+// => the {args[i].length} construct returns the number of accepted function arguments
+
+const getPosition = overload(
+// @ts-ignore
+	[{x: 'Number', y: 'Number'},
+		function (x, y) {
+			console.log(x, y)
+		}],
+
+	// @ts-ignore
+
+	function (str, num, str2) {
+		console.log(str, num, str2)
+	}
+);
+
+getPosition(1, 2)
+
+
+/*
 
 function overload() {
 	const args: Array<TOverloadArguments> = Array.from<TOverloadArguments>(arguments);
@@ -15,16 +87,4 @@ function overload() {
 	}
 }
 
-const getPosition = overload(
-// @ts-ignore
-	
-	function (x, y) {
-		console.log(x, y)
-	}
-	,
-	function (str, num, str2) {
-		console.log(str, num, str2)
-	}
-);
-
-getPosition(1, 2, 3)
+* */
