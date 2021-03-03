8e50b8006c1862802bdb1dfd3e2e30f18431f5aa
Vladislav Adamets <51527608+adametsofficial@users.noreply.github.com>
2021-03-04 00:18:15 +0200

    Add type checker for items in array

--- dist/functionOverload.js
+++ dist/functionOverload.js
@@ -1,46 +1,72 @@
-export function overload() {
+function overload() {
     const args = Array.from(arguments);
     let functions = [];
+    
     for (let i = 0; i !== args.length; i++) {
         if (typeof(args[i]) === 'function') {
             functions[args[i].length] = args[i];
-        }
-        if (args[i] instanceof Array) {
+        } else if (args[i] instanceof Array) {
             const argArr = args[i];
+            
             if (argArr[0] instanceof Object && argArr[1] instanceof Function) {
                 functions[args[i][1].length] = args[i];
             }
+        } else { 
+            continue;
         }
     }
+    
     return function() {
         const callElement = functions[arguments.length];
+        
         if (callElement instanceof Function) {
             return functions[arguments.length].apply(this, arguments);
-        } else if (callElement instanceof Array) {
+            
+        } else if (callElement instanceof Array && callElement.length === 2) {
             const typesOfCallElement = Object.values(callElement[0]);
-            let call = false;
             let mathingTypes = [];
+
+            if (!(callElement[0] instanceof Object)) {
+                throw new SyntaxError('First argument of array must be a type Object.')
+            }
+
+            if (!(callElement[1] instanceof Function)) {
+                throw new SyntaxError('Second argument of array must be a type Function.')
+            }
 
             for (let i = 0; i !== Array.from(arguments).length; i++) {
 
                 if (typeof typesOfCallElement[i] !== 'string') {
-                    console.log(new SyntaxError('Type must be a string!'));
                     mathingTypes = ['0'];
-                    break;
+                    throw new SyntaxError('Type must be a string!')
                 }
 
                 mathingTypes = [...mathingTypes, typeof(Array.from(arguments)[i]) === typesOfCallElement[i].toLowerCase() ?
                     '1' : '0'
                 ]
             }
-
+            
             const isMatchingTypes = mathingTypes.find((i) => i === "0");
 
             if (isMatchingTypes === undefined) {
                 return functions[arguments.length][1].apply(this, arguments);
             } else {
-                console.error(SyntaxError(`You have a mistake in your arguments type!`));
+                throw new SyntaxError(`You have a mistake in your arguments type!`);
             }
+        } else {
+            throw new SyntaxError('Argument must be type Function or Array of Object with arguments type and function with that arguments.');
         }
     };
 }
+
+const multifunc = overload(
+function(x, y) { return x + y },
+function (x, y, z) { return x + y + z },
+
+[{x: 'Number'}, function(x) {return x}]
+
+);
+
+console.log(multifunc(1, 2))
+console.log(multifunc(1, 2, 3))
+console.log(multifunc(1))
