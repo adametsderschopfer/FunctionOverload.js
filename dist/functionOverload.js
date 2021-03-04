5b48b5e1dfff5f3d1baf98a61152d7ffdf29f4cc
Vladislav Adamets <51527608+adametsofficial@users.noreply.github.com>
2021-03-05 00:38:16 +0200

    feet: add function for getting inner function arguments and check with object types

--- dist/functionOverload.js
+++ dist/functionOverload.js
@@ -14,6 +14,20 @@ function overload()
         
         return true
     }
+    
+    function getArgs(func) {
+      // First match everything inside the function argument parens.
+      var args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
+
+      // Split the arguments string into an array comma delimited.
+      return args.split(',').map(function(arg) {
+        // Ensure no inline comments are parsed and trim the whitespace.
+        return arg.replace(/\/\*.*\*\//, '').trim();
+      }).filter(function(arg) {
+        // Ensure no undefined values are added.
+        return arg;
+      });
+    }
 
     const args = Array.from(arguments);
     let functions = [];
